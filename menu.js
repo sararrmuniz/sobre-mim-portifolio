f7f0d4f26fdc70f60e9cd5095ef1ba9094d67d5e
sarrmuniz <sararozado@hotmail.com>
2026-09-22 10:58:52 -0300

    Portifólio atualizado

--- menu.js
+++ menu.js
@@ -8,3 +8,60 @@ btnMenu.addEventListener('click',()=>{
 menu.addEventListener('click',()=>{
     menu.classList.remove('abrir-menu')
 })
+
+const cards = document.querySelectorAll(".especialidades-box");
+const indicators = document.querySelectorAll(".indicator");
+const previousButton = document.querySelector(".carousel-prev");
+const nextButton = document.querySelector(".carousel-next");
+
+let currentCard = 0;
+
+function showCard(index) {
+cards.forEach((card) => {
+card.classList.remove("active");
+});
+
+
+indicators.forEach((indicator) => {
+    indicator.classList.remove("active");
+});
+
+cards[index].classList.add("active");
+indicators[index].classList.add("active");
+
+currentCard = index;
+
+
+}
+
+nextButton.addEventListener("click", () => {
+let nextCard = currentCard + 1;
+
+
+if (nextCard >= cards.length) {
+    nextCard = 0;
+}
+
+showCard(nextCard);
+
+
+});
+
+previousButton.addEventListener("click", () => {
+let previousCard = currentCard - 1;
+
+
+if (previousCard < 0) {
+    previousCard = cards.length - 1;
+}
+
+showCard(previousCard);
+
+
+});
+
+indicators.forEach((indicator, index) => {
+indicator.addEventListener("click", () => {
+showCard(index);
+});
+});
